dade63c52768d43d8a16da86af118ef0fc303579
ManishSaini-369 <manishsaini1340@gmail.com>
2025-04-24 12:30:51 +0530

    odoo branding remover.

--- home_dashboard_oi/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import models
-from .models.install import _post_init_hook 
--- home_dashboard_oi/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import odi_home_page
-from . import odi_hp_config
-from . import odi_hp_setting
